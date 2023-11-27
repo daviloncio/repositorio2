303d9bc642630e8ee39bf8ffef56880630bb4491
daviloncio <davidlv1909@gmail.com>
2023-11-27 11:59:20 +0100

    c

---
+++ p3_aa/Plantilla_NaiveBayes.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+class NaiveBayes:
+  
+    # Constructor: pasar argumentos necesarios
+  def __init__(self, laplace=False,...):   
+    pass
+  
+  # Calcula el modelo correspondiente a partir de los datos de train
+  # TODO: implementar
+  def fit(self,X,y):
+	pass
+    
+  # devuelve el conjunto de clases estimadas para cada uno de los datos de test
+  # TODO: implementar
+  def predict(self,X):
+	pass  
+
+  # devuelve el accuracy medio, es decir, los aciertos en las predicciones del conjunto pasado como argumento
+  # TODO: implementar
+  def score(self,X,y):
+	pass  
